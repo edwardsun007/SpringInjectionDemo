245ea1b1488a5f1be61be4d2428037e269fa9af4
Edward Sun <esun@expediagroup.com>
2021-10-07 00:13:11 -0700

    Imported Spring initializer generated file and setup it as maven project

---
+++ src/test/java/guru/springframework/sfgdiproject/SfgDiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgdiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
