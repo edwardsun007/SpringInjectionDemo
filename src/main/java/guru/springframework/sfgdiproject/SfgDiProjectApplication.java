e3b4bdfb3561f381ecd1513bfb6e6015d6e8e04a
Edward Sun <esun@expediagroup.com>
2021-10-12 18:15:05 -0700

    Added controller and demo of Application Context

--- src/main/java/guru/springframework/sfgdiproject/SfgDiProjectApplication.java
+++ src/main/java/guru/springframework/sfgdiproject/SfgDiProjectApplication.java
@@ -1,13 +1,20 @@
 package guru.springframework.sfgdiproject;
 
+import guru.springframework.sfgdiproject.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiProjectApplication.class, args);
+		ApplicationContext ctx =  SpringApplication.run(SfgDiProjectApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController"); // This is how we ask Spring context for controller, the name start with lower case note
+		String greeting = myController.sayHi();
+
+		System.out.println(greeting);
 	}
 
 }
