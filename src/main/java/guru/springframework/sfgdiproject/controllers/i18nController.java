259cbf2387ad29d18e280fd15e8d2624bde8c95e
Edward Sun <esun@expediagroup.com>
2021-10-21 22:10:22 -0700

    Default Profile lecture explained

---
+++ src/main/java/guru/springframework/sfgdiproject/controllers/i18nController.java
@@ -0,0 +1,19 @@
+package guru.springframework.sfgdiproject.controllers;
+
+import guru.springframework.sfgdiproject.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+
+@Controller
+public class i18nController {
+    private final GreetingService greetingService;
+
+    public i18nController(@Qualifier("i18nService") GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello() {
+        return greetingService.sayGreeting();
+    }
+}
